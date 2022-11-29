bb27d89512b002702d6afd934b622ba8fa62045d
Seelam Sushma <seesushm@publicisgroupe.net>
2022-11-29 22:24:25 +0530

    Added

---
+++ src/test/java/com/sapient/FirstClass.java
@@ -0,0 +1,11 @@
+package com.sapient;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		String firstname="Sushma";
+		String lastname=" Seelam";
+		System.out.println("Hello "+firstname.concat(lastname));
+	}
+
+}
